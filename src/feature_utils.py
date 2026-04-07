fdc7da8e280bd5e45e943ac346ffe91dcb7fbe1b
Will Kirk <williamgkirk@icloud.com>
2026-04-07 18:05:07 -0500

    Update feature_utils.py

--- src/feature_utils.py
+++ src/feature_utils.py
@@ -104,9 +104,9 @@ def convert_input_pca_regression(request_body, request_content_type):
         X = FeatureEngineer(windows=[10,15]).transform(dataset[[target]])
     
         techIndicator_1 = 'EMA_10'
-        RSI_15 = json.loads(request_body)[techIndicator_1]
+        EMA_10 = json.loads(request_body)[techIndicator_1]
         techIndicator_2 = 'EMA_15'
-        MOM_15 = json.loads(request_body)[techIndicator_2]
+        EMA_15 = json.loads(request_body)[techIndicator_2]
 
         # Calculate the distance
         distances = np.sqrt(
